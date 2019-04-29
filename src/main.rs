40de4d67e71619fad38b4a135d50d2db865ca0dd
Malte Tammena <malte.tammena@gmx.de>
2019-04-29 12:24:03 +0200

    Clock-dependent caching working

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,127 @@
+use std::cell::RefCell;
 use std::ops::DerefMut;
 use std::rc::Rc;
-use std::cell::RefCell;
 
-#[derive(Debug)]
-struct Block<F, F1, F2> {
+trait Input {
+    type Output;
+    fn out(&mut self, clock: usize) -> &Self::Output;
+}
+
+#[derive(Debug, Clone)]
+struct Block2<F, I1, I2, O>
+where
+    I1: Input,
+    I2: Input,
+    F: FnMut(usize, &I1::Output, &I2::Output) -> O,
+{
     id: String,
-    in1: Rc<RefCell<Input<F1>>>,
-    in2: Rc<RefCell<Input<F2>>>,
+    last_clock: usize,
+    last_result: Option<O>,
+    in1: Rc<RefCell<I1>>,
+    in2: Rc<RefCell<I2>>,
     f: Box<F>,
 }
 
 #[derive(Debug, Clone)]
-struct Input<F> {
+struct RawInput<F, O>
+where
+    F: FnMut(usize) -> O,
+{
     id: String,
+    last_clock: usize,
+    last_result: Option<O>,
     f: Box<F>,
 }
 
 fn main() {
-    let in1 = Input::new("Always TRUE", || {
-        println!("IN1: {}", true);
-        true
+    let in1 = RawInput::new("Always TRUE", |c| {
+        let now = c % 4 == 2 || c % 4 == 3;
+        print!("CLOCK: {}, IN1 {},\t", c, now);
+        now
     });
-    let in2 = Input::new("Always FALSE", || {
-        println!("IN1: {}", false);
-        false
+    let in2 = RawInput::new("Always FALSE", |c| {
+        let now = c % 2 == 1;
+        print!("IN2 {},\t", now);
+        now
     });
-    let mut and = Block::new("AND", in1.clone(), in2.clone(), |x, y| x && y);
-    let mut or = Block::new("AND", in1.clone(), in2.clone(), |x, y| x || y);
+    let and1 = Block2::new("AND_1", in1.clone(), in2.clone(), |_, &x, &y| x && y);
+    let and2 = Block2::new("AND_2", in1.clone(), in2.clone(), |_, &x, &y| !x && !y);
+    let and3 = Block2::new("EQUALS", and1.clone(), and2.clone(), |_, &x, &y| x || y);
 
-    println!("AND {}", and.out());
-    println!("OR  {}", or.out());
+    for clock in 0..4 {
+        println!("EQUALS {}", and3.borrow_mut().out(clock));
+    }
 }
 
-impl<F, O> Input<F>
+impl<F, O> RawInput<F, O>
 where
-    F: FnMut() -> O,
+    F: FnMut(usize) -> O,
 {
     fn new(id: &str, f: F) -> Rc<RefCell<Self>> {
         let f = Box::from(f);
         let id = id.into();
-        Rc::new(RefCell::new(Input { id, f } ))
-    }
-
-    fn out(&mut self) -> O {
-        (*self.f.deref_mut())()
+        let last_clock = 0;
+        let last_result = None;
+        Rc::new(RefCell::new(RawInput {
+            id,
+            f,
+            last_clock,
+            last_result,
+        }))
     }
 }
 
-impl<F, F1, F2> Block<F, F1, F2>
+impl<F, I1, I2, O> Block2<F, I1, I2, O>
 where
-    F: FnMut(bool, bool) -> bool,
-    F1: FnMut() -> bool,
-    F2: FnMut() -> bool,
+    F: FnMut(usize, &I1::Output, &I2::Output) -> O,
+    I1: Input,
+    I2: Input,
 {
-    fn new(id: &str, in1: Rc<RefCell<Input<F1>>>, in2: Rc<RefCell<Input<F2>>>, f: F) -> Self
-    where
-        F: FnMut(bool, bool) -> bool,
-    {
+    fn new(id: &str, in1: Rc<RefCell<I1>>, in2: Rc<RefCell<I2>>, f: F) -> Rc<RefCell<Self>> {
         let id = id.into();
-        Block {
+        let last_clock = 0;
+        let last_result = None;
+        Rc::new(RefCell::new(Block2 {
             id,
             f: f.into(),
             in1,
             in2,
-        }
+            last_clock,
+            last_result,
+        }))
     }
+}
 
-    fn out(&mut self) -> bool {
-        let mut in1 = self.in1.borrow_mut();
-        let mut in2 = self.in2.borrow_mut();
-        (*self.f.deref_mut())(in1.out(), in2.out())
+impl<F, O> Input for RawInput<F, O>
+where
+    F: FnMut(usize) -> O,
+{
+    type Output = O;
+
+    fn out(&mut self, clock: usize) -> &Self::Output {
+        if self.last_clock != clock || self.last_result.is_none() {
+            self.last_clock = clock;
+            self.last_result = Some(self.f.deref_mut()(clock));
+        }
+        self.last_result.as_ref().unwrap()
+    }
+}
+
+impl<F, I1, I2, O> Input for Block2<F, I1, I2, O>
+where
+    I1: Input,
+    I2: Input,
+    F: FnMut(usize, &I1::Output, &I2::Output) -> O,
+{
+    type Output = O;
+
+    fn out(&mut self, clock: usize) -> &O {
+        if self.last_clock != clock || self.last_result.is_none() {
+            self.last_clock = clock;
+            let mut in1 = self.in1.borrow_mut();
+            let mut in2 = self.in2.borrow_mut();
+            self.last_result = Some(self.f.deref_mut()(clock, &in1.out(clock), &in2.out(clock)));
+        }
+        self.last_result.as_ref().unwrap()
     }
 }
