f8484e86c8b4266d09d7bbb5f72de2a0ccba1bbc
Malte Tammena <malte.tammena@gmx.de>
2019-06-08 19:17:31 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,44 @@
-use pretty_env_logger;
+//! # Emulator for Minirechner 2a microcomputer
+//!
+//! ## Usage
+//! ```usage
+//! 2a-emulator 0.1.0
+//! Malte Tammena <malte.tammena@gmx.de>
+//! Emulator for the Minirechner 2a microcomputer
+//!
+//! USAGE:
+//!     emulator-2a [FLAGS] [OPTIONS] [--] [PROGRAM]
+//!
+//! FLAGS:
+//!     -c, --check          Validate the given source file
+//!     -h, --help           Prints help information
+//!     -i, --interactive    Start an interactive session
+//!     -V, --version        Prints version information
+//!
+//! OPTIONS:
+//!     -t, --test <TEST>...    Specify a test file
+//!
+//! ARGS:
+//!     <PROGRAM>    File to load and verify
+//! ```
 
-use std::io;
+
+use pretty_env_logger;
 
 pub mod schematic;
 pub mod tui;
+pub mod cli;
 
-fn main() -> Result<(), io::Error> {
+use std::process;
+
+fn main() {
     pretty_env_logger::init();
 
-    tui::run()
+    match cli::handle_user_input() {
+        Err(e) => {
+            println!("{}", e);
+            process::exit(1)
+        }
+        _ => {}
+    }
 }
