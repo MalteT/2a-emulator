159c4790488570d675dffda323881831d732cb4f
Malte Tammena <malte.tammena@pm.me>
2022-03-23 11:47:35 +0100

    Simplify flake.nix

--- flake.nix
+++ flake.nix
@@ -1,52 +1,21 @@
 {
-  inputs = {
-    pkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nmattia/naersk";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-  };
+  inputs.nixCargoIntegration.url = "github:yusdacra/nix-cargo-integration";
 
-  outputs = { self, utils, naersk, ... }@inputs:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pname = "2a-emulator";
+  outputs = inputs:
+    inputs.nixCargoIntegration.lib.makeOutputs {
+      root = ./.;
+      renameOutputs = {
+        "emulator-2a" = "2a-emulator";
+        "emulator-2a-lib" = "2a-emulator-lib";
+      };
+      defaultOutput.app = "2a-emulator";
+      defaultOutput.package = "2a-emulator";
 
-        overlays = [ inputs.rust-overlay.overlay ];
-        pkgs = import inputs.pkgs { inherit system overlays; };
+      enablePreCommitHooks = true;
 
-        # Get the latest rust nightly
-        rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
-          toolchain.default.override { extensions = [ "rust-src" ]; });
-
-        # Override the version used in naersk
-        naersk-lib = naersk.lib."${system}".override {
-          cargo = rust;
-          rustc = rust;
-        };
-      in rec {
-        # `nix build`
-        packages.${pname} = naersk-lib.buildPackage {
-          inherit pname;
-          root = ./.;
-        };
-        defaultPackage = packages.${pname};
-
-        # `nix run`
-        apps.${pname} = utils.lib.mkApp { drv = packages.${pname}; };
-        defaultApp = apps.${pname};
-
-        # `nix develop`
-        devShell = pkgs.mkShell {
-          # supply the specific rust version
-          nativeBuildInputs = [ rust pkgs.cargo-readme ];
-          RUST_SRC_PATH = "${rust}";
-        };
-
-        # `nix check`
-        checks.${pname} = naersk-lib.buildPackage {
-          inherit pname;
-          root = ./.;
-          doCheck = true;
-        };
-      });
+      cachix = {
+        name = "2a-emulator";
+        key = "2a-emulator.cachix.org-1:ijJDEqNsMqhamxxWvqOiaCQNoYhWNw7A+gGICgAH1mE=";
+      };
+    };
 }
