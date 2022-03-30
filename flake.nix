71d8a765dc243ffe313cbbd45245830f7819b49f
Malte Tammena <malte.tammena@pm.me>
2022-03-30 21:50:36 +0200

    fix: Pin flake input to stable branch

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixCargoIntegration.url = "github:yusdacra/nix-cargo-integration";
+  inputs.nixCargoIntegration.url = "github:yusdacra/nix-cargo-integration/release-1.0";
 
   outputs = inputs:
     inputs.nixCargoIntegration.lib.makeOutputs {
@@ -8,14 +8,15 @@
         "emulator-2a" = "2a-emulator";
         "emulator-2a-lib" = "2a-emulator-lib";
       };
-      defaultOutput.app = "2a-emulator";
-      defaultOutput.package = "2a-emulator";
+      defaultOutputs.app = "2a-emulator";
+      defaultOutputs.package = "2a-emulator";
 
-      enablePreCommitHooks = true;
+      #enablePreCommitHooks = true;
 
-      cachix = {
-        name = "2a-emulator";
-        key = "2a-emulator.cachix.org-1:ijJDEqNsMqhamxxWvqOiaCQNoYhWNw7A+gGICgAH1mE=";
-      };
+      # TODO: Reenable when updating nix-cargo-integration
+      # cachix = {
+      #   name = "2a-emulator";
+      #   key = "2a-emulator.cachix.org-1:ijJDEqNsMqhamxxWvqOiaCQNoYhWNw7A+gGICgAH1mE=";
+      # };
     };
 }
